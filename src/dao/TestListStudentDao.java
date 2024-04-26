f2502e10e46d38f35d7df6c384d43340805afcb6
Yuki <fki2366286@stu.o-hara.ac.jp>
2024-04-26 14:49:53 +0900

    Added 4/26/'24

---
+++ src/dao/TestListStudentDao.java
@@ -0,0 +1,76 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Student;
+
+public class TestListStudentDao extends Dao{
+
+	private String baseSql = "select * from student where school_cd=? ";
+
+	private List<TestListStudent> postFilter(ResultSet rSet) throws Exception {
+		List<Student> list = new ArrayList<>();
+		try {
+			while (rSet.next()) {
+				Student student = new Student();
+				student.setNo(rSet.getString("no"));
+				student.setName(rSet.getString("name"));
+				student.setEntYear(rSet.getInt("ent_year"));
+				student.setClassNum(rSet.getString("class_num"));
+				student.setAttend(rSet.getBoolean("is_attend"));
+				student.setSchool(school);
+				list.add(student);
+			}
+		} catch (SQLException | NullPointerException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<TestListStudent> filter(Student student) throws Exception {
+		List<Student> list = new ArrayList<>();
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+		ResultSet rSet = null;
+		String condition = "and ent_year=? and class_num=?";
+		String order = " order by no asc";
+		String conditionIsAttend = "";
+		if (isAttend) {
+			conditionIsAttend = "and is_attend=true";
+		}
+		try {
+			statement = connection.prepareStatement(baseSql + condition + conditionIsAttend + order);
+			statement.setString(1, school.getCd());
+			statement.setInt(2, entYear);
+			statement.setString(3, classNum);
+			rSet = statement.executeQuery();
+			list = postFilter(rSet, school);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+
+		return list;
+	}
+
+
+}
