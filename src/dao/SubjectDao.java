1e8306551b3b2bfb3b3ecd34d04044ab8c91805e
oohara <oohara@DESKTOP-GMMMUGQ>
2024-04-30 10:04:09 +0900

    :

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -5,13 +5,17 @@ import java.util.List;
 import bean.School;
 import bean.Subject;
 
-public interface SubjectDao {
+public interface SubjectDao{
 
     // 科目コードと学校から科目を取得する
     Subject get(String cd, School school);
 
     // 学校に関連付けられたすべての科目をフィルタリングする
-    List<Subject>postFilter(school School) throws Exception {
-    }
-    
+    List<Subject> filter(School school);
+
+    // 科目を保存する
+    boolean save(Subject subject);
+
+    // 科目を削除する
+    boolean delete(Subject subject);
 }
