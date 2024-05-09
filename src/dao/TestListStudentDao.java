854362ef69d4ab54ee453f9275932c7387d30417
oohara <oohara@172.16.31.69>
2024-05-09 14:35:21 +0900

    2024/05/09/14:35

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -2,59 +2,33 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import bean.School;
 import bean.Student;
-import bean.Subject;
 
 public class TestListStudentDao extends Dao{
 
-	private String baseSql = "select * from student where school_cd=? ";
+	private String baseSql = "SELECT * FROM STUDENT";
 	School school = new School();
 
-	private List<Subject> postFilter(ResultSet rSet) throws Exception {
-
-		List<Subject> list = new ArrayList<>();
-
-		try {
-			while (rSet.next()) {
-				Subject subject = new Subject();
-				subject.setCd(rSet.getString("no"));
-			}
-		} catch (SQLException | NullPointerException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-
 	public List<Student> filter(Student student) throws Exception {
-		List<Subject> list = new ArrayList<>();
+		List<Student> list = new ArrayList<>();
 		Connection connection = getConnection();
 		PreparedStatement statement = null;
-		String entYearStr="";
 		String classNum="";
-		String isAttendStr="";
 		int entYear = 0;
-		LocalDate todaysDate = LocalDate.now();
-		int year = todaysDate.getYear();
 
-		ResultSet rSet = null;
-		String condition = "and ent_year=? and class_num=?";
-		String order = " order by no asc";
+		String condition = "JOIN TEST ON STUDENT.NO = TEST.STUDENT_NO;";
 		String conditionIsAttend = "";
 
 		try {
-			statement = connection.prepareStatement(baseSql + condition + conditionIsAttend + order);
+			statement = connection.prepareStatement(baseSql + condition + conditionIsAttend);
 			statement.setString(1, school.getCd());
 			statement.setInt(2, entYear);
 			statement.setString(3, classNum);
-			rSet = statement.executeQuery();
-			list = postFilter(rSet);
 		} catch (Exception e) {
 			throw e;
 		} finally {
