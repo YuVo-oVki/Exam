0972f30efd0875aa8ac9828f97a11be1195f7a44
oohara <oohara@DESKTOP-PTQMSBQ>
2024-05-10 14:26:50 +0900

    2024/05/10

--- src/scoremanager/main/SubjectCreateExecuteAction.java
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bean.School;
 import bean.Subject;
+import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
@@ -17,29 +17,30 @@ public class SubjectCreateExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		//ローカル変数の宣言 1
-		HttpSession session = req.getSession();//セッション
-		SubjectDao subDao = new SubjectDao();//科目Dao
-		String subCd ="";		//科目コード
-		String subName = "";	//科目名
-		Subject subjects = null;//科目
+		//HttpSession session = req.getSession();//セッション
+		SubjectDao sDao = new SubjectDao();//科目Dao
+		String cd ="";		//科目コード
+		String name = "";	//科目名
+		Subject subject = null;//科目
 		Map<String, String> errors = new HashMap<>();// エラーメッセージ
 
 		School school=new School();
 		school.setCd("oom");
 		school.setName("学校名");
 
-		Subject subject = new Subject();
-		subject.setSchool(school);
-		subject.setCd("oom");
-		subject.setName("国語");
+		Teacher teacher = new Teacher();
+		teacher.setId("admin");
+		teacher.setPassword("password");
+		teacher.setName("大原花子");
+		teacher.setSchool(school);
 
 		//リクエストパラメータ―の取得 2
-		subCd = req.getParameter("subCd");//科目コード
-		subName = req.getParameter("subName");//科目名
+		cd = req.getParameter("cd");//科目コード
+		name = req.getParameter("name");//科目名
 
 
 		//DBからデータ取得 3
-		subjects = subDao.get(subCd, school);//科目インスタンス
+		subject = sDao.get(cd,school);//科目インスタンス
 
 
 
@@ -47,32 +48,31 @@ public class SubjectCreateExecuteAction extends Action {
 		//DBへデータ保存 5
 		//条件で手順4~5の内容が分岐
 
-		if (subCd == null) {// 科目コードが未登録だった場合
-			// 学生インスタンスを初期化
-			 subjects= new Subject();
+
+		if (subject == null) {// 科目が未登録だった場合
+			// 科目インスタンスを初期化
+			subject = new Subject();
 			// インスタンスに値をセット
+			subject.setCd(cd);
+			subject.setName(name);
 
-			subjects.setSchool(school);
-			subjects.setCd(subCd);
-			subjects.setName(subName);
-
-			// 学生を保存
-			subDao.save(subjects);
-		} else {//入力された科目コードがDBに保存されていた場合
-			errors.put("subCd", "科目コードが重複しています");
+			// 科目を保存
+			sDao.save(subject);
+		} else {//入力された学番がDBに保存されていた場合
+			errors.put("cd", "学生番号が重複しています");
 		}
 
 
-		//エラーがあったかどうかで手順6~7の内容が分岐
-		//レスポンス値をセット 6
+
+
 		//JSPへフォワード 7
 
 
 		if(!errors.isEmpty()){
 			// リクエスト属性をセット
 			req.setAttribute("errors", errors);
-			req.setAttribute("sub_cd", subCd);
-			req.setAttribute("sub_name", subName);
+			req.setAttribute("cd", cd);
+			req.setAttribute("name", name);
 			req.getRequestDispatcher("subject_create.jsp").forward(req, res);
 			return;
 		}
